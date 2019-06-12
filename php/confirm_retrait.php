22801fb49ee77b395fee67635dc2ff22dfbf5d58
Sabayke <36015556+Sabayke@users.noreply.github.com>
2019-06-12 18:07:43 +0000

    Add files via upload

--- php/confirm_retrait.php
+++ php/confirm_retrait.php
@@ -17,13 +17,20 @@ include 'my_db.php';
 		// si les 2 champs : montant_saisie et code ne sont pas vide 
 			if($montant_saisie==$montant){
 				//on verifie  le montant
-				$verif_requete = $bdd->prepare("SELECT * FROM code_retrait WHERE Num_Code= '" . $code . "' LIMIT 1");
+				$verif_requete = $bdd->prepare("SELECT Num_Code FROM code_retrait WHERE Num_Code= '" . $code . "' LIMIT 1");
 				$verif_requete->execute(array($code));
 				$etat_code = $verif_requete->rowCount();
 				// verifions si le code existe
 				if($etat_code==1){
+					// verification si le code est déjà utilisé
+					$verif_requetes = $bdd->prepare("SELECT Etat FROM code_retrait WHERE Num_Code= '" . $code . "' LIMIT 1");
+					$verif_requetes->execute(array($code));
+						$utilisation = $verif_requetes->fetch()[0];
+						//var_dump($utilisation);
+						if($utilisation == 'inutilisé')
+						{
 					/* gérer la validité du code */
-							$requete_date = $bdd->prepare("SELECT Date_de_creation FROM code_retrait WHERE Num_Code= '". $code ."' LIMIT 1");
+							$requete_date = $bdd->prepare("SELECT Date_de_Creation FROM code_retrait WHERE Num_Code= '". $code ."' LIMIT 1");
 							$requete_date->execute();
 							$date_de_creation = $requete_date->fetch()[0]; //date de création du code
 							// recupérons le code et stockons le dans la variable $date_de_creation
@@ -33,7 +40,7 @@ include 'my_db.php';
 							$date_du_serveur = time(); // date du serveur
 							//recupérons la date du serveur
 							$date_limite = 2*24*60*60; // date d'éxpiration du code
-							if($date_du_serveur-$date_debut==$date_limite)
+							if($date_du_serveur-$date_debut>=$date_limite )
 								{ // si la difference est <= 2jours
 									// le code est invalide
 									$expire= "expiré";
@@ -57,7 +64,9 @@ include 'my_db.php';
 																echo "Félicitation le retrait à etait fait avec succés";
 											}
 					
-	
+	}else{
+		echo "le code est déjà utilisé";
+	}
 					
 					}else{
 						echo "le code est incorrect";}
--- php/retrait.php
+++ php/retrait.php
@@ -19,7 +19,7 @@ include 'my_db.php';
 	}else
 	{
 		//si oui on verifie si le numéro exist ou pas 
-		$requete1 = $bdd->prepare("SELECT NumTel FROM user WHERE NumTel= '" . $numtel . "' LIMIT 1");
+		$requete1 = $bdd->prepare("SELECT Num_Tel FROM utilisateur WHERE Num_Tel= '" . $numtel . "' LIMIT 1");
 		$requete1->execute(array($numtel));
 		//var_dump($requete1);
 		$etat_num = $requete1->rowCount();
@@ -27,14 +27,14 @@ include 'my_db.php';
 		{
 			//le numéro exist
 						//vérification du solde
-						//$login est une variable session 
-						$login = "sabayke";
-						$solde = $bdd->prepare("SELECT Solde FROM compte WHERE Id_User_Login= '". $login ."' LIMIT 1");
+						
+						$solde = $bdd->prepare("SELECT `Solde` FROM `compte` INNER JOIN `Utilisateur` on `compte`.`Id_User`=`utilisateur`.`Login` WHERE `Num_Tel`='".$numtel."' LIMIT 1");
 						$solde->execute(array($numtel));
 						$solde_user = $solde->fetch()[0];
 						//var_dump();
 		
 						//si oui le montant doit être positive et le solde est supérieur au montant rétiré
+						if(!empty($montant)){
 						if($montant>=1 && $solde_user>$montant)
 						{
 								// frais de rétrait =25Fcfa c'est pour tester 
@@ -45,34 +45,45 @@ include 'my_db.php';
 									$code_generator = random_int(1000, 9999);
 									// random_int() une fonction qui génére un code aléatoire et unique
 									//on affiche le code avec echo 
-									echo "votre code est : $code_generator";
 									// on a fixé les ID pour simplifier notre travail
-									$id_compte1= "sabayke";
-									$id_compte2= "bremso";
+									/* les clés etrangers sont id_compte1 et id_compte2
+									SELECT `Solde` FROM `compte` INNER JOIN `transaction` on `compte`.`Id_Compte`=`compte`.`Id_Compte` WHERE `Id_Compte_1`='1' LIMIT 1;*/
+									$compte= $bdd->prepare("SELECT `Id_Compte_1` FROM `transaction` INNER JOIN `compte` on `transaction`.`Id_Compte_1`=`compte`.`Id_Compte` WHERE `Solde`='".$solde_user."' LIMIT 1");
+											$compte->execute(array($numtel));
+												$id_compte = $compte->fetch()[0];
 									$services=   "retrait";
 									// on insére les données dans la table transactions
+									$id_transaction = random_int(1, 100);
 									$transaction_retrait= $bdd->prepare("INSERT INTO transaction 
-									(Id_Compte1,Id_compte2,Type_Transaction,frais_Transaction,montant)
-										VALUES( '". $id_compte1 ."', '". $id_compte2 ."','". $services ."', '". $frais1 ."', '". $montant ."' )");
-									$transaction_retrait->execute(array($id_compte1,$id_compte2,$services,$frais1,$montant));
+									(Id_Transaction,Id_Compte_1,Id_Compte_2,Type_Transaction,Frais_Transaction,Montant)
+										VALUES('".$id_transaction."','". $id_compte ."', '". $id_compte ."','". $services ."', '". $frais1 ."', '". $montant ."' )");
+
+									$transaction_retrait->execute(array($id_transaction,$id_compte,$id_compte,$services,$frais1,$montant));
 									$Etat= "inutilisé";
 									// on insére les données dans la table code_retrait
-									$insertclient = $bdd->prepare("INSERT INTO code_retrait(Num_Code, Id_Client, Etat, Montat) VALUES('". $code_generator ."', '". $id_compte1 ."', '". $Etat ."', '". $montant ."')");
-									$insertclient->execute(array($code_generator, $id_compte1, $Etat, $montant));
+									/* Id_client clé etrangers*/
+									/*$id_client="sabayke";*/
+									$id_client_num = $bdd->prepare("SELECT Login FROM Utilisateur WHERE Num_Tel= '".$numtel."' LIMIT 1");
+									$id_client_num->execute(array($numtel));
+									$Id_client_login=$id_client_num->fetch()[0];
+									$id=random_int(1, 30);
+									$insertclient = $bdd->prepare("INSERT INTO code_retrait(Id, Num_Code, Id_Client, Etat, Montat) VALUES('".$id."', '". $code_generator ."', '". $Id_client_login ."', '". $Etat ."', '". $montant ."')");
+									$insertclient->execute(array($code_generator, $Id_client_login, $Etat, $montant));
 									// on fait une mise à jour à la table compte solde = solde -transaction-frais
 									$mise_a_jour3 = $bdd->prepare("
 									UPDATE compte
-									SET Solde = Solde-'". $montant ."'-'". $frais1 ."' WHERE Id_User_Login= '". $login ."' LIMIT 1");
+									SET Solde = Solde-'". $montant ."'-'". $frais1 ."' WHERE Id_User= '". $Id_client_login ."' LIMIT 1");
 										$mise_a_jour3->execute(array($montant));
 									//montant est une variable session pour faire une verification
 									$_SESSION['montant']= $montant;
-
+									echo "votre code est : $code_generator";
 						}else{
 						    echo "Votre solde ne vous permez pas d'effectuer cette opération";
 							}
 								}else{
 									echo "Veuillez charger votre solde pour effectuer cette opération";
 									}
+								}else{echo "Veuillez renseigner le montant";} 
 		}else {
 			echo "Mauvais numéro !!! avez vous un compte !";
 			}
